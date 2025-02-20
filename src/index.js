288a589b8e8b2dfb41ef8914437d004f3da3ec98
dax-side <damolaadegbite0@gmail.com>
2025-02-20 20:04:31 +0100

    refactor: update path handling for remote nginx server logs and (adding integration.json directly in endpoint)

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const LogParser = require("./logParser");
 const config = require("./config");
+
 const cors = require('cors'); // Add this line
 
 function createApp() {
